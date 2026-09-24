ec2a03689c8961a8787cb7198ad3cc322fd6f1e7
2311cs010110 <2311cs010110@mallareddyuniversity.ac.in>
2026-09-24 14:03:06 +0530

    recent commit

--- server/app.js
+++ server/app.js
@@ -1,28 +1,25 @@
-const express = require("express");
-const connectDB = require("./config/db");
-const dotenv = require("dotenv");
-const cors = require("cors");
+const express = require("express")
+const connectDB = require("./config/db")
+const dotenv = require("dotenv")
+const dns = require("dns")
+const courseRoute = require("./routes/courseRoute")
+const authRoute = require("./routes/authRoutes")
 
-const courseRoute = require("./routes/courseRoute");
-const authRoute = require("./routes/authRoutes");
+const app = express()
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 
-app.use(cors({
-  origin: "http://localhost:5173"
-}));
+app.use(express.json())
 
-app.use(express.json());
+app.use("/api/auth", authRoute)
+app.use("/api/courses", courseRoute)
 
-app.use("/api/auth", authRoute);
-app.use("/api/courses", courseRoute);
+connectDB()
 
-connectDB();
-
-const PORT = 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on port ${PORT}`)
+})
