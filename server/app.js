abce21b0f0320c6a4c3723616a83f4890965af1a
2311cs010110 <2311cs010110@mallareddyuniversity.ac.in>
2026-09-25 22:14:02 +0530

    lAtest

--- server/app.js
+++ server/app.js
@@ -1,20 +1,28 @@
 const express = require("express")
 const connectDB = require("./config/db")
 const dotenv = require("dotenv")
-const dns = require("dns")
 const courseRoute = require("./routes/courseRoute")
 const authRoute = require("./routes/authRoutes")
+const cors = require("cors")
 
 const app = express()
 
 dotenv.config()
 
-dns.setServers(["1.1.1.1", "8.8.8.8"])
-
 app.use(express.json())
 
-app.use("/api/auth", authRoute)
+app.use(cors({
+    origin: "https://remarkable-swan-1764a6.netlify.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true
+}))
+
+app.get("/welcome", (req, res) => {
+    res.send("Welcome Back")
+})
+
 app.use("/api/courses", courseRoute)
+app.use("/api/auth", authRoute)
 
 connectDB()
 
